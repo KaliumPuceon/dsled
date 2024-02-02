5b15733da46888199d58dff508384c4d44ca2f85
Kali Singh <jehan.singh@suse.com>
2024-02-02 20:17:37 +0100

    Expression evaluator functions

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,111 @@
 use std::collections::VecDeque;
 
+#[derive(Clone, Debug)]
 struct Block {
 
-    operation: fn(Vec<Term>),       // Function call for this operation
-    children: Vec<Term>,            // Argterms of this term
+    operation: fn(VecDeque<Term>) -> Result<Term, String>,       // Function call for this operation
+    children: VecDeque<Term>,            // Argterms of this term
 
 }
 
+#[derive(Clone, Debug)]
 enum Term {
 
     Constant(f32),
     Var(String),
-    Subterm(Vec<Block>),
+    Subterm(Block),
 
 }
 
+fn compute(mut block: Block) -> Term{
 
-fn mul(terms: Vec<Term>) -> Result<Block,String> {
-    todo!();
+    let solved_children = block.children.iter_mut();
+    solved_children.for_each(|term| {
+
+        if let Term::Subterm(subterm_block) = term {
+
+            *term = compute(subterm_block.clone());
+
+        }
+
+    });
+    return (block.operation)(block.children).unwrap();
+
 }
 
-fn div(terms: Vec<Term>) -> Result<Block,String> {
-    todo!();
+fn mul(mut terms: VecDeque<Term>) -> Result<Term,String> {
+    
+    if let Some(Term::Constant(mut acc)) = terms.pop_front() {
+        while let Some(Term::Constant(x)) = terms.pop_front(){
+            acc *= x;
+        }
+    return Ok(Term::Constant(acc))
+    }
+    Err("Multiply failed".to_string())
 }
 
-fn add(terms: Vec<Term>) -> Result<Block,String> {
-    todo!();
+fn div(mut terms: VecDeque<Term>) -> Result<Term,String> {
+    if let Some(Term::Constant(numerator)) = terms.pop_front() {
+        if let Some(Term::Constant(denominator)) = terms.pop_front() {
+            return Ok(Term::Constant(numerator/denominator))
+        }
+    }
+    Err("Divide failed failed".to_string())
 }
 
-fn sub(terms: Vec<Term>) -> Result<Block,String> {
-    todo!();
+fn add(mut terms: VecDeque<Term>) -> Result<Term,String> {
+
+    if let Some(Term::Constant(mut acc)) = terms.pop_front() {
+        while let Some(Term::Constant(x)) = terms.pop_front(){
+            acc += x;
+        }
+    return Ok(Term::Constant(acc))
+    }
+    Err("Add failed failed".to_string())
+
 }
 
-fn sin(terms: Vec<Term>) -> Result<Block,String> {
-    todo!();
+fn sub(mut terms: VecDeque<Term>) -> Result<Term,String> {
+
+    if let Some(Term::Constant(mut acc)) = terms.pop_front() {
+        while let Some(Term::Constant(x)) = terms.pop_front(){
+            acc -= x;
+        }
+    return Ok(Term::Constant(acc))
+    }
+    Err("Sub failed".to_string())
+
 }
 
-fn cos(terms: Vec<Term>) -> Result<Block,String> {
-    todo!();
+fn sin(mut terms: VecDeque<Term>) -> Result<Term,String> {
+    if let Some(Term::Constant(x)) = terms.pop_front() {
+        return Ok(Term::Constant(x.sin()))
+    }
+    Err("Sine failed".to_string())
 }
 
-fn tan(terms: Vec<Term>) -> Result<Block,String> {
-    todo!();
+fn cos(mut terms: VecDeque<Term>) -> Result<Term,String> {
+    if let Some(Term::Constant(x)) = terms.pop_front() {
+        return Ok(Term::Constant(x.cos()))
+    }
+    Err("Cosine failed".to_string())
 }
 
-fn abs(terms: Vec<Term>) -> Result<Block,String> {
-    todo!();
+fn tan(mut terms: VecDeque<Term>) -> Result<Term,String> {
+    if let Some(Term::Constant(x)) = terms.pop_front() {
+        return Ok(Term::Constant(x.tan()))
+    }
+    Err("Tangent failed".to_string())
 }
 
-const OPS:  [( &str, fn(Vec<Term>) -> Result<Block,String> ); 8] = [
+fn abs(mut terms: VecDeque<Term>) -> Result<Term,String> {
+    if let Some(Term::Constant(x)) = terms.pop_front() {
+        return Ok(Term::Constant(x.abs()))
+    }
+    Err("Abs failed".to_string())
+}
+
+const OPS:  [( &str, fn(VecDeque<Term>) -> Result<Term ,String> ); 8] = [
     ("*", mul),
     ("/", div),
     ("+", add),
@@ -62,17 +119,15 @@ const OPS:  [( &str, fn(Vec<Term>) -> Result<Block,String> ); 8] = [
 // Take in a Vec of Strings, and break it up into a vec of Terms which contain their own
 // vecs of Terms. If something is not a subterm, just append it to the current running vec of
 // termvals. If you're entering a new subterm, split it out and parse that recursively.
-fn process_term(opstring: &mut VecDeque<&str>) -> Result<(Vec<Term>, i32), String> {
-
-    let mut expression: Vec<Term>;
+fn process_term(opstring: &mut VecDeque<&str>) -> Result<Block, String> {
 
     let mut before_term = true;
     let mut in_term = false;
     let mut end_of_term = false;
     let mut found_op = false;
 
-    let mut result_op: fn(Vec<Term>) -> Result<Block, String>;
-    let mut result_children: Vec<Term>;
+    let mut result_op: Option<fn(VecDeque<Term>) -> Result<Term, String>> = None;
+    let mut result_children: VecDeque<Term> = vec![].into();
 
     while !end_of_term || before_term {
 
@@ -93,40 +148,61 @@ fn process_term(opstring: &mut VecDeque<&str>) -> Result<(Vec<Term>, i32), Strin
                 opstring.push_front("(");
                 println!("Passing down {:?}", opstring);
                 if let Ok(subterm) = process_term(opstring) {
-                    
+                    result_children.push_back(Term::Subterm(subterm));
                 }
             }
 
             else if let Some(matched_fun) = OPS.iter().find(|op| (op.0 == next)) {
-                println!("matched on a function {}", matched_fun.0);
-                result_op = matched_fun.1;
+                if !found_op {
+                    println!("matched on a function {}", matched_fun.0);
+                    result_op = Some(matched_fun.1);
+                    found_op = true;
+                }
+                else {
+
+                    return Err("Too many operators in term".to_string())
+
+                }
             }
             else {
 
                 if let Ok(num) = next.parse::<f32>() {
+
+                    result_children.push_back(Term::Constant(num));
                     
                 }
 
             }
-
             println!("{}",next);
         }
         else 
         {
 
-            end_of_term = true;
             return Err("Badly matched parens somewhere".to_string())
         
         }
 
     }
 
-    println!("end of term, returning up");
-    Err("Not Working Yet".to_string())
+    if let Some(_) = result_op{
+
+        let result_block = Block {
+
+            operation: result_op.unwrap(),
+            children: result_children,
+
+        };
+        println!("end of term, returning up {:?}", result_block);
+        return Ok(result_block);
+
+    }
+    else {
+        return Err("Didn't work :(".to_string())
+    }
 
 }
 
-fn parse(code: &str) -> Result<(Vec<Term>, i32), String>  {
+fn parse(code: &str) -> Result<Block, String>  {
 
     /*
      * * Strip newlines and split on whitespace and ( ) to create list of token strings
@@ -150,21 +226,26 @@ fn parse(code: &str) -> Result<(Vec<Term>, i32), String>  {
     let lisp = code.to_string();
     let mut sliced = lisp.split(&[' ']).collect();
 
-    if let Err(output) = process_term(&mut sliced) {
+    if let Ok(output) = process_term(&mut sliced) {
 
-        println!("Error {}", output);
+        println!("calls: {:?}", output);
+        return Ok(output);
 
     }
-
+    
 
     Err("Not Working Yet".to_string())
 
 }
 
 fn main() {
-    //let input = "( + 3 ( * 2 3 ) 4 5 ( sin 0.3 ) )";
-    let input = "( + 13 ( - 4 5 ) )";
+   // let input = "( + 3 (* 2 4 ) )";
+    let input = "( + 13 ( - 4 5 ) ( sin 0.3 ) ( cos ( * 2 0.56 ) ) )";
     println!("parsing {}", input);
-    parse(input);
+    let computable_blocks = parse(input);
     println!("Parsing code {}", input);
+    let output = compute(computable_blocks.unwrap());
+
+    println!("{:?}", output);
+
 }
